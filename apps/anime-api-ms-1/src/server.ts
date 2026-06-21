8314686e4fa7d38dab381aac25d904327af831a2
Udaas Cheenti <systum@haager.com>
2026-06-21 15:20:34 +0000

    feat: added basic structure of anime api

---
+++ apps/anime-api-ms-1/src/server.ts
@@ -0,0 +1,10 @@
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env["PORT"] || 3001;
+
+app.listen(port, () => {
+    console.log(`Anime server is running on port ${port}`);
+});
